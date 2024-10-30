7b679ab58b96eed430d8b2555dd52ed37ec16754
Dmitriy <babakov_da4@mail.ru>
2024-10-30 23:09:25 +0500

    Me descr

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Dima";
-
-            Console.WriteLine(myName);
-
-            Console.ReadKey();
+            string name = "Dima";
+            byte age = 19;
+            bool animal = true;
+            byte sizeleg = 44;
+            
+            Console.WriteLine("My name is {0}\nMy age is {1}\nDo I have a pet? {2}\nMy shoe size is {3}", name, age, animal, sizeleg);
         }
     }
 }
