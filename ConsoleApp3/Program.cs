75643f35edd1663ee5845d0c4e38eb42afdd135f
Dmitriy <babakov_da4@mail.ru>
2024-10-30 22:23:07 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Jane";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
