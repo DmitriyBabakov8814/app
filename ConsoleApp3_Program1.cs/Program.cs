fe3b47b7aa45a9d2c0feb9b0c3262bd8d8421f95
Dmitriy <babakov_da4@mail.ru>
2024-11-07 22:48:51 +0500

    add do while in Program1.cs

--- ConsoleApp3_Program1.cs/Program.cs
+++ ConsoleApp3_Program1.cs/Program.cs
@@ -4,10 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int t = 1;
+            int t = 0;
 
             do
             {
+                Console.WriteLine("Цикл do while");
                 Console.WriteLine("Iteration {0}", t);
                 switch (Console.ReadLine())
                 {
@@ -38,7 +39,45 @@
                 t++;
             }
 
-            while (t < 4);
+            while (t < -1);
+
+            Console.WriteLine("Цикл while");
+            int k = 0;
+
+            while (k < -1)
+            {
+                Console.WriteLine(k);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+
+                }
+
+                k++;
+            }
         }
     }
 }
