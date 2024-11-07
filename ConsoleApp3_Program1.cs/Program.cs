f485f177f833bfd36d88b66c1ebfe4f61d5eb147
Dmitriy <babakov_da4@mail.ru>
2024-11-07 21:58:45 +0500

    New File

--- ConsoleApp3_Program1.cs/Program.cs
+++ ConsoleApp3_Program1.cs/Program.cs
@@ -1,61 +1,44 @@
-﻿using System.Xml.Linq;
-
-namespace ConsoleApp3
+﻿namespace ConsoleApp3_Program1.cs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int t = 1;
 
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-
-
-            for (int i = 5; i > 1; i--)
+            do
             {
-                Console.WriteLine("Iteration {0}", i);
+                Console.WriteLine("Iteration {0}", t);
                 switch (Console.ReadLine())
                 {
                     case "red":
                         Console.BackgroundColor = ConsoleColor.Red;
                         Console.ForegroundColor = ConsoleColor.Black;
-
                         Console.WriteLine("Your color is red!");
                         break;
 
                     case "green":
                         Console.BackgroundColor = ConsoleColor.Green;
                         Console.ForegroundColor = ConsoleColor.Black;
-
                         Console.WriteLine("Your color is green!");
                         break;
 
                     case "cyan":
                         Console.BackgroundColor = ConsoleColor.Cyan;
                         Console.ForegroundColor = ConsoleColor.Black;
-
                         Console.WriteLine("Your color is cyan!");
                         break;
                     default:
                         Console.BackgroundColor = ConsoleColor.Yellow;
                         Console.ForegroundColor = ConsoleColor.Black;
-
                         Console.WriteLine("Your color is yellow!");
                         break;
 
                 }
+                t++;
             }
+
+            while (t < 4);
         }
     }
-}
-
-enum DayOfWeek : byte
-{
-    Monday = 1,
-    Tuesday, 
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
 }
