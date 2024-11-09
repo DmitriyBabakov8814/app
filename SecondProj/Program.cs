452676756403776c01500ce1a935ff000d364f35
Dmitriy <babakov_da4@mail.ru>
2024-11-10 00:32:01 +0500

    anketa

--- SecondProj/Program.cs
+++ SecondProj/Program.cs
@@ -4,47 +4,53 @@
     {
         static void Main(string[] args)
         {
+
             (string name, string surname, string login, int lenlogin, bool pet, double age, string[] favcolors) User;
 
-            Console.WriteLine("Введите имя");
-
-            User.name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию");
-
-            User.surname = Console.ReadLine();
-
-            Console.WriteLine("Введите логин");
-
-            User.login = Console.ReadLine();
-
-            User.lenlogin = User.login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-
-            if (Console.ReadLine() == "Да")
+            for (int i = 0; i < 3; i++)
             {
-                User.pet = true;
+                
+                Console.WriteLine("Введите имя");
+
+                User.name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+
+                User.surname = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+
+                User.login = Console.ReadLine();
+
+                User.lenlogin = User.login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+
+                if (Console.ReadLine() == "Да")
+                {
+                    User.pet = true;
+                }
+                else
+                {
+                    User.pet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+
+                User.age = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                User.favcolors = new string[3];
+
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
             }
-            else
-            {
-                User.pet = false;
-            }
-
-            Console.WriteLine("Введите возраст пользователя");
-
-            User.age = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите три любимых цвета пользователя");
-
-            User.favcolors = new string[3];
-
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                string color = Console.ReadLine();
-                User.favcolors[i] = color;
-                Console.WriteLine(User.favcolors[i]);
-            }
+            
+            
+            
 
         }
     }
