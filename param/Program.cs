2312a9d694416d98ece709fc322ef096e6740901
Dmitriy <babakov_da4@mail.ru>
2024-11-14 23:47:08 +0500

    add new file param

---
+++ param/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace param
+{
+    class Program
+    {
+        static void ShowColors(string name = "Dima", params string[] favcolors)
+        {
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string[] favcolors = new string[3] { "Красный", "Синий", "Зеленый" };
+
+            ShowColors();
+        }
+    }
+}
