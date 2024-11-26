dc27137ef532b6edd11b419785aec0eb2200c24b
Dmitriy <babakov_da4@mail.ru>
2024-11-27 02:02:49 +0500

    add get features

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,74 +1,88 @@
-﻿using System.Runtime.Intrinsics.Arm;
+﻿using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.Arm;
 
 namespace oop
 {
-    class Company
-    {
-        public string Type;
-        public string Name;
-    }
 
-    class Department
+    class User
     {
-        public Company company;
-        public City city;
-    }
+        private int age;
+        private string login;
+        private string email;
 
-    class City
-    {
-        public string Name;
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if(value.Contains('@'))
+                {
+                    email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Не содержит @");
+                }
+                
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                if(value.Length >= 3)
+                {
+                    login = value;
+                }
+                else
+                {
+                    Console.WriteLine("Не менее 3х символов");
+                }
+            }
+        }
+
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if(value < 18)
+                {
+                    Console.WriteLine("нет 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Department department = GetCurrentDepartment();
+            User user = new User();
 
-            if (department != null)
-            {
-                Console.WriteLine("У банка {0} есть отделение в {1}", department.company.Name, department.city.Name);
-
-            }
-
-        }
-
-        static Department GetCurrentDepartment()
-        {
-            Department dep = new Department
-            {
-                company = new Company { Name = "тесла", Type = "Банк" },
-                city = new City { Name = null }
-            }; 
-            
-            
-
-
-            if (dep.company.Type != null && dep.city.Name != null)
-            {
-                if (dep.company.Type == "Банк" && dep.city.Name == "Питер")
-                {
-
-                   return dep;
-
-                }
-
-                else
-                {
-                    Console.WriteLine("Неизвестная компания");
-                    
-                }
-            }
-            else
-            {
-                Console.WriteLine("Значения пустые");
-            }
-            return null;
-
-
-            
+            user.Login = "s24";
+            user.Email = "dsm@akdsma";
         }
     }
-
 
 }
